23c3bc8e9a8fdbde8ce90f39cb8f3a6ec11477d2
giovana1806 <betareligiovana@gmail.com>
2024-05-23 14:29:16 -0300

    JPA avaliacao boxblue

---
+++ src/main/java/projetojpaAvBoxBlue/ProjetojpaBoxBlueApplication.java
@@ -0,0 +1,13 @@
+package projetojpaAvBoxBlue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetojpaBoxBlueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetojpaBoxBlueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/projetojpaAvBoxBlue/ProjetojpaBoxBlueApplicationTests.java
@@ -0,0 +1,13 @@
+package projetojpaAvBoxBlue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetojpaBoxBlueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
